414e08d81b5a3dae086fca644e7d700c36070d7e
yatsenko.v <FDT3sRtwD>
2023-10-31 18:53:37 +0300

    add migrations

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
 	implementation("org.modelmapper:modelmapper:3.2.0")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("io.micrometer:micrometer-registry-prometheus")
+	implementation("org.flywaydb:flyway-core:9.22.3")
+
 
 	compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
